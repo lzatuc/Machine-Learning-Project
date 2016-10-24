6cd812801ac2d3c1cf01dd1ea00ba7f6fe1ef393
Li Zheng <li@clari.com>
2016-10-24 01:59:44 -0700

    add NaiveBayes

--- digit_recognizer/KNN.py
+++ digit_recognizer/KNN.py
@@ -1,41 +1,12 @@
 import queue
 import math
+from FileIOManager import FileIOManager
 
 
 class KNN:
-
-    def __init__(self, training_file, testing_file):
-        self.file_paths = ["", ""]
-        self.file_paths[0] = training_file
-        self.file_paths[1] = testing_file
-        self.labels = []
-        self.training_set = []
-        self.testing_set = []
-
-    def __parse_line(self, line, idx):
-        pixels = line.split(",")
-        if idx == 0:
-            self.labels.append(int(pixels[0]))
-            pixels = pixels[1:]
-        sparse_rep = []
-        feature_len = len(pixels)
-        for i in range(feature_len):
-            val = int(pixels[i])
-            if val != 0:
-                sparse_rep.append((val, i))
-        if idx == 0:
-            self.training_set.append(sparse_rep)
-        else:
-            self.testing_set.append(sparse_rep)
-
-    def __read_file(self):
-        for idx in range(2):
-            file = open(self.file_paths[idx])
-            file.readline()
-            line = file.readline()
-            while line:
-                self.__parse_line(line, idx)
-                line = file.readline()
+    def __init__(self, training_path, testing_path):
+        self.file_io_manager = FileIOManager(training_path, testing_path)
+        self.file_io_manager.read_file()
 
     @staticmethod
     def __dist(vec1, vec2):
@@ -67,17 +38,16 @@ class KNN:
             idx2 += 1
         return math.sqrt(similarity)
 
-    def knn(self):
-        self.__read_file()
+    def knn(self,):
         k = 10
         target_labels = []
-        for testing_case in self.testing_set:
+        for testing_case in self.file_io_manager.testing_set:
             vote = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
             que = queue.PriorityQueue()
-            for i in range(len(self.training_set)):
-                training_case = self.training_set[i]
+            for i in range(len(self.file_io_manager.training_set)):
+                training_case = self.file_io_manager.training_set[i]
                 similarity = self.__dist(training_case, testing_case)
-                que.put((similarity, self.labels[i]))
+                que.put((similarity, self.file_io_manager.labels[i]))
             for i in range(k):
                 elem = que.get()
                 vote[elem[1]] += 1
@@ -86,7 +56,7 @@ class KNN:
             target_labels.append(label)
         return target_labels
 
+
 if __name__ == '__main__':
     target_labels = KNN('train.csv', 'test.csv').knn()
     print(target_labels)
-
